4312da9e2c4629404fbad511f62892081344b13b
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-02-01 19:44:21 +0530

    [ADDED]:[Vikas] Swastik desging using tab and next line.

---
+++ SWASTIK.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ clrscr();
+ printf("*\t*********\n");
+ printf("*\t*\n");
+ printf("*\t*\n");
+ printf("*\t*\n");
+ printf("*\t*\n");
+ printf("*****************\n");
+ printf(" \t*\t*\n");
+ printf(" \t*\t*\n");
+ printf(" \t*\t*\n");
+ printf(" \t*\t*\n");
+ printf("*********\t*\n");
+
+ getch();
+}
